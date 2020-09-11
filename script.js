6ebf646df1982c36f5bc49033e1d91d3d81bcdf9
mariosz96 <mariosgpt@gmail.com>
2020-09-11 12:17:11 +0300

    First commit for JS Calculator

---
+++ script.js
@@ -0,0 +1,111 @@
+let numButtons = document.getElementsByClassName("calc-num");
+let opButtons = document.getElementsByClassName("calc-operator");
+let clrBtn = document.getElementById("calc-clear");
+let backspace = document.getElementById("calc-bcsp");
+let displayNum = document.getElementById("numDisplay");
+let allButtons = document.getElementsByClassName("calc-btn");
+let decimal = document.getElementById("calc-decimal");
+
+let valDisplay = "0";
+let store = "";
+let evalArray = [];
+let lastButtonPressedWasEquals = false;
+
+if (typeof String.prototype.trim === "undefined") {
+  String.prototype.trim = function () {
+    return String(this).replace(/^\s+|\s+$/g, "");
+  };
+}
+
+function calculation(array) {
+  finalString = math.eval(array.join(" ").trim());
+
+  return finalString;
+}
+
+function updateDisplay(e) {
+  if (lastButtonPressedWasEquals === true) {
+    valDisplay = "";
+    lastButtonPressedWasEquals = false;
+  }
+
+  let btnText = e.target.innerText;
+
+  if (valDisplay === "0") {
+    valDisplay = "";
+  }
+
+  valDisplay += btnText;
+  displayNum.value = valDisplay;
+}
+
+function performOperation(e) {
+  let operator = e.target.innerText;
+
+  switch (operator) {
+    case "+":
+      store = valDisplay;
+      valDisplay = "0";
+      displayNum.value = valDisplay;
+      evalArray.push(store, "+");
+      break;
+
+    case "-":
+      store = valDisplay;
+      valDisplay = "0";
+      displayNum.value = valDisplay;
+      evalArray.push(store, "-");
+      break;
+
+    case "⨯":
+      store = valDisplay;
+      valDisplay = "0";
+      displayNum.value = valDisplay;
+      evalArray.push(store, "*");
+      break;
+
+    case "÷":
+      store = valDisplay;
+      valDisplay = "0";
+      displayNum.value = valDisplay;
+      evalArray.push(store, "/");
+      break;
+
+    case "=":
+      evalArray.push(valDisplay);
+      valDisplay = calculation(evalArray);
+      displayNum.value = valDisplay;
+      evalArray = [];
+      lastButtonPressedWasEquals = true;
+      break;
+  }
+}
+
+for (let i = 0; i < numButtons.length; i++) {
+  numButtons[i].addEventListener("click", updateDisplay);
+}
+
+for (let i = 0; i < opButtons.length; i++) {
+  opButtons[i].addEventListener("click", performOperation);
+}
+
+clrBtn.addEventListener("click", () => {
+  valDisplay = "0";
+  store = "0";
+  evalArray = [];
+  displayNum.value = "0";
+});
+
+backspace.addEventListener("click", () => {
+  valDisplay = valDisplay.toString().slice(0, valDisplay.length - 1);
+  displayNum.value = valDisplay;
+  if (valDisplay === "") {
+    displayNum.value = "0";
+  }
+});
+
+decimal.addEventListener("click", () => {
+  if (!valDisplay.includes(".")) {
+    valDisplay += ".";
+  }
+});
